2248df7809e703ac20aacb2cdfeee143562784d7
SDBLiveProject <sdb.hvd1111@gmail.com>
2023-10-21 12:52:18 +0530

    NavbarFIx

--- src/components/client/layouts/Header.js
+++ src/components/client/layouts/Header.js
@@ -1,35 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
 export default function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <>
       <Helmet>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap"
-          rel="stylesheet"
-        />
-
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet" />
         <link href="assets/client/css/bootstrap.min.css" rel="stylesheet" />
-
         <link href="assets/client/css/bootstrap-icons.css" rel="stylesheet" />
-
         <link href="assets/client/css/owl.carousel.min.css" rel="stylesheet" />
-
-        <link
-          href="assets/client/css/owl.theme.default.min.css"
-          rel="stylesheet"
-        />
-
-        <link
-          href="assets/client/css/templatemo-medic-care.css"
-          rel="stylesheet"
-        />
+        <link href="assets/client/css/owl.theme.default.min.css" rel="stylesheet" />
+        <link href="assets/client/css/templatemo-medic-care.css" rel="stylesheet" />
       </Helmet>
       <nav className="navbar navbar-expand-lg bg-light fixed-top shadow-lg">
         <div className="container">
@@ -41,16 +31,18 @@ export default function Header() {
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
+            onClick={toggleNav}
             aria-controls="navbarNav"
-            aria-expanded="false"
+            aria-expanded={isNavOpen}
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div
+            className={`collapse navbar-collapse${isNavOpen ? ' show' : ''}`}
+            id="navbarNav"
+          >
             <ul className="navbar-nav mx-auto">
               <li className="nav-item">
                 <Link to="/" className="nav-link">
